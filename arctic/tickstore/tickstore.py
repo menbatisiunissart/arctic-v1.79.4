80de802fbe4f518859586a8885be5e5536322729
menbatisiunissart <aboulenger@gmail.com>
2022-10-30 22:30:13 +0000

    fix: updated "_arrays_to_mgr" logic

--- arctic/tickstore/tickstore.py
+++ arctic/tickstore/tickstore.py
@@ -353,7 +353,7 @@ class TickStore(object):
 
         t = (dt.now() - perf_start).total_seconds()
         logger.info("Got data in %s secs, creating DataFrame..." % t)
-        mgr = _arrays_to_mgr(arrays, columns, index, columns, dtype=None)
+        mgr = _arrays_to_mgr(arrays, columns, index, dtype=None, typ="array")
         rtn = pd.DataFrame(mgr)
         # Present data in the user's default TimeZone
         rtn.index = rtn.index.tz_convert(mktz())
